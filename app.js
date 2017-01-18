b736cccdc0c6eb21fc25e00f728e163a6bbf9517
Anis Ben Amara <anisba@allmas-tn.com>
2017-01-18 18:59:45 +0100

    Run fist server

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-var assert = require('assert');
+var express = require('express');
+var engines = require('consolidate');
 var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var app = express();
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
   assert.equal(err, null);
   console.log('Successfully connected to server');
 
-  // find some documents
-  // db.collection('movies').find({}).toArray(function(err, docs) {
-  //
-  //   // title of each document
-  //   docs.forEach(function(doc) {
-  //     console.log(doc.title);
-  //   });
-  //
-  //   db.close();
-  // });
-
-  db.collection('movies').find({}, function(err, docs) {
-
-    // title of each document
-
-    docs.forEach(function(doc) {
-      console.log(doc);
-    })
-
-    db.close();
+  app.get('/', function(req, res) {
+    db.collection('movies').find({}).toArray(function(err, docs) {
+      res.render('index', {'movies': docs});
+    });
   });
 
-  console.log('Called find()');
-});
+  app.use(function(req, res) {
+    res.sendStatus(404);
+  });
+
+  var server = app.listen(8000, function() {
+    var port = server.address().port;
+    console.log('Express server listening on port %s', port);
+  });
+ });
