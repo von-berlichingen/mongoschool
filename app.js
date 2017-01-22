fbd5c0b69f360b1c9979ec90be844c5a15342b35
von-berlichingen <anis.b.amara@gmail.com>
2017-01-22 20:53:53 +0100

    Form with node and consolidate

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 var express = require('express');
 var engines = require('consolidate');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(errorHandler);
 
-MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
-  assert.equal(err, null);
-  console.log('Successfully connected to server');
+app.get('/', function(req, res, next) {
+    res.render('index', { 'fruits' : [ 'apple', 'orange', 'banana', 'peach' ] });
+});
 
-  app.get('/', function(req, res) {
-    db.collection('movies').find({}).toArray(function(err, docs) {
-      res.render('index', {'movies': docs});
-    });
-  });
+app.post('/favorite_fruit', function(req, res, next) {
+    var favorite = req.body.fruit;
+    if (typeof favorite == 'undefined') {
+        next('Please choose a fruit!');
+    }
+    else {
+        res.send("Your favorite fruit is " + favorite);
+    }
+});
 
-  app.get('/:name', function(req, res) {
-    var name = req.params.name;
-    var val1 = req.query.val1;
-    var val2 = req.query.val2;
-    console.log(val1);
-
-    res.render('index', {'name': name, 'val1': val1, 'val2': val2})
-  })
-
-  app.use(function(req, res) {
-    res.sendStatus(404);
-  });
-
-  var server = app.listen(8000, function() {
+var server = app.listen(8000, function() {
     var port = server.address().port;
-    console.log('Express server listening on port %s', port);
-  });
- });
+    console.log('Express server listening on port %s.', port);
+});
+
+/////////////////////
+
+// Handler for internal server errors
+function errorHandler(err, req, res, next) {
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500).render('error_template', { error: err });
+}
